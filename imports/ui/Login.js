8c66ffbd55a9da4d57f0462bab7a582bd6b012d5
Roman Priscepov <roman.priscepov@hotmail.com>
2017-09-18 13:57:22 +0300

    Update login form language

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -38,7 +38,7 @@ export default class Login extends React.Component {
                         <input type="password" ref="password" name="password" placeholder="Password"/>
                         <button className="button">Login</button>
                     </form>
-                    <Link to="/signup">Have an account?</Link>
+                    <Link to="/signup">Need an account?</Link>
                 </div>
             </div>
         );
